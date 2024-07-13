cae922e9d725bbc14dd72f0f7cc173b905159c34
aman3255 <95523243+aman3255@users.noreply.github.com>
2024-07-13 10:51:13 +0530

    Create Context.jsx

---
+++ Context.jsx
@@ -0,0 +1,47 @@
+import { useContext, useState } from "react";
+import { CountContext } from "./context";
+
+function App() { // Top level component
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
+
+    </div>
+  )
+}
+function Count({ setCount }) { // First child.
+  return (
+    <div>
+      <CountRenderer />
+      <Button setCount={setCount} />
+    </div>
+  )
+}
+function CountRenderer() {
+  const count = useContext(CountContext);
+  return (
+    <div>
+      {count}
+    </div>
+  )
+}
+function Button({ setCount }) {
+  const count = useContext(CountContext);
+  return (
+    <div>
+      <button onClick={() => {
+        setCount(count + 1);
+      }}>Increase</button>
+      <button onClick={() => {
+        setCount(count - 1);
+      }}>Decrease</button>
+    </div>
+  )
+
+}
+
+
+export default App;
